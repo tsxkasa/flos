3365babf17b5770565feec173fc4efb06959110e
tsxkasa <62455208+tsxkasa@users.noreply.github.com>
2026-05-02 20:21:02 -0700

    fixed hcf returning
    added attrib [[noreturn]] to hcf()

--- src/kernel/arch/x86_64/cpu/halt.c
+++ src/kernel/arch/x86_64/cpu/halt.c
@@ -1,7 +1,1 @@
 #include <cpu/halt.h>
-
-void hcf(void) {
-  for (;;) {
-    asm("hlt");
-  }
-}
--- src/kernel/arch/x86_64/cpu/halt.h
+++ src/kernel/arch/x86_64/cpu/halt.h
@@ -1,6 +1,10 @@
 #ifndef _KERNEL_HALT_H
 #define _KERNEL_HALT_H
 // Halt and catch fire function.
-void hcf(void);
+[[noreturn]] void hcf(void) {
+  for (;;) {
+    asm("hlt");
+  }
+}
 
 #endif // K_HALT_H
