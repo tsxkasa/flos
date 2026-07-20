ad87f627139269ef7f8cac457a454cd34abc4679
tsxkasa <62455208+tsxkasa@users.noreply.github.com>
2026-07-19 20:44:19 -0700

    slight change on name in asm offsets

--- src/kernel/arch/x86_64/asm-offsets.h
+++ src/kernel/arch/x86_64/asm-offsets.h
@@ -3,11 +3,12 @@
 
 #include <mm/pmap/arch_pmap_types.h>
 #include <mm/vm/vm_map.h>
-#include <sched/thread.h>
+#include <sched/task.h>
 #include <stddef.h>
 
-#define OFFSET_PT_PHY        offsetof(struct page_table_t, pml4_phy)
-#define OFFSET_TASK_STACKPTR offsetof(task_t, stack_ptr)
-#define OFFSET_TASK_VMMAP    offsetof(task_t, vmap)
+#define OFFSET_PT_PHY     offsetof(struct page_table_t, pml4_phy)
+#define OFFSET_TASK_STACK offsetof(task_t, stack)
+#define OFFSET_TASK_VMMAP offsetof(task_t, vmap)
+#define OFFSET_VM_MAP_PT  offsetof(vm_map_t, page_table)
 
 #endif // _ASM_OFFSETS_H
--- src/kernel/tools/gen_offsets/main.c
+++ src/kernel/tools/gen_offsets/main.c
@@ -5,8 +5,9 @@ int main() {
   printf("%%ifndef __ASM_OFFSETS_NASM_INC\n");
   printf("%%define __ASM_OFFSETS_NASM_INC\n");
   printf("%%define OFFSET_PT_PHY %zu\n", OFFSET_PT_PHY);
-  printf("%%define OFFSET_TASK_STACKPTR %zu\n", OFFSET_TASK_STACKPTR);
+  printf("%%define OFFSET_TASK_STACK %zu\n", OFFSET_TASK_STACK);
   printf("%%define OFFSET_TASK_VMMAP %zu\n", OFFSET_TASK_VMMAP);
+  printf("%%define OFFSET_VM_MAP_PT %zu\n", OFFSET_VM_MAP_PT);
   printf("%%endif");
 
   return 0;
