b477108e5f708e4ec9cad864a12fa31993c9f3cd
tsxkasa <62455208+tsxkasa@users.noreply.github.com>
2026-08-07 23:21:56 -0700

    kernel - to virt/phys
    added a kernel relative phys/virt

--- src/kernel/mm/address.h
+++ src/kernel/mm/address.h
@@ -14,4 +14,14 @@ static inline uintptr_t higher_half_data_to_phys(uintptr_t address) {
   return address - boot_get_hhdm_offset();
 }
 
+static inline uintptr_t kernel_virt_to_phys(uintptr_t address) {
+  kernel_addr_t k = boot_get_executable_addr();
+  return k.phys + (address - k.virt);
+}
+
+static inline uintptr_t kernel_phys_to_virt(uintptr_t address) {
+  kernel_addr_t k = boot_get_executable_addr();
+  return k.virt + (address - k.phys);
+}
+
 #endif // _KERNEL_ARCH_MEMORY_H
